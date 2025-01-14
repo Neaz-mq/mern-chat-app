e5655c499e71864270f545f862e8023ab7f518d9
neazmorshed666@gmail.com <neazmorshed666@gmail.com>
2025-01-14 11:44:51 +0600

    user auth working

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import messageRoutes from "./routes/message.route.js";
 
 const app = express();
 import authRoutes from "./routes/auth.route.js";
@@ -7,10 +9,13 @@ import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001; // Use 5002 instead of 5001
+
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
     console.log("server is running on PORT:" + PORT);
