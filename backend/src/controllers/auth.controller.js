53f7f25f82c4e8fdedfe1bd2b653044da2484306
neazmorshed666@gmail.com <neazmorshed666@gmail.com>
2025-01-14 15:19:35 +0600

    updated files and cors

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,10 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
-export const signup = async (req,res) => {
-    const { fullName, email, password } = req.body;
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
   try {
     if (!fullName || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
@@ -48,12 +49,7 @@ export const signup = async (req,res) => {
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
-
   try {
-    if (!email || !password) {
-      return res.status(400).json({ message: "All fields are required" });
-    }
-
     const user = await User.findOne({ email });
 
     if (!user) {
@@ -61,12 +57,11 @@ export const login = async (req, res) => {
     }
 
     const isPasswordCorrect = await bcrypt.compare(password, user.password);
-
     if (!isPasswordCorrect) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
 
-    generateToken(user._id, res); // Set JWT in a cookie
+    generateToken(user._id, res);
 
     res.status(200).json({
       _id: user._id,
@@ -75,11 +70,10 @@ export const login = async (req, res) => {
       profilePic: user.profilePic,
     });
   } catch (error) {
-    console.error("Error in login controller", error.message);
+    console.log("Error in login controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
-
 
 export const logout = (req, res) => {
   try {
@@ -122,4 +116,3 @@ export const checkAuth = (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
-
